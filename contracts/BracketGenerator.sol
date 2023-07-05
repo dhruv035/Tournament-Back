0c979c13ad80ea86d0c590fe881515502f65aeff
dhruv035 <dhruv035@gmail.com>
2023-07-06 00:10:09 +0530

    gasReduce

--- contracts/BracketGenerator.sol
+++ contracts/BracketGenerator.sol
@@ -3,128 +3,121 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-
 contract BracketGenerator {
-
-   struct Team {
-      uint256 id;
-      string name;
-   }
-   struct Match {
-      uint256 id;
-      uint256 teama;
-      uint256 teamb;
-      uint256 winner;
-   }
-   struct Bracket {
-
-      address creator;
-      Match[] matchups;
-      string name;
-      Team[] participants;
-      uint256 initial;
-      uint256 winner;
-   }
-
-
-   function shuffle(string[] memory arr) internal view returns(string[] memory) {
-    for (uint256 i = 0; i < arr.length; i++) {
-        uint256 n = i + uint256(keccak256(abi.encodePacked(block.timestamp))) % (arr.length - i);
-        string memory temp = arr[n];
-        arr[n] = arr[i];
-        arr[i] = temp;
+    struct Match {
+        uint256 teama;
+        uint256 teamb;
+        uint256 winner;
     }
-    return arr;
-}
-   mapping (address=>string[])tournaments;
-   mapping(string=>Bracket) internal db;
-   function viewTournament (string memory name) public view returns (Bracket memory){
-      return db[name];
+    struct Team {
+        uint256 id;
+        string name;
+    }
+    struct Bracket {
+        string name;
+        Team[] participants;
+        mapping(uint256=>Match)matchups;
+        uint256 initial;
+        uint256 preskipped;
+        uint256 winner;
+        uint256 visible;
+    }
+    mapping(address => mapping(string => Bracket)) internal db;
+    mapping(address => string[]) internal tournaments;
+
+   function getTournamentWinner (string memory name, address owner) public view returns (string memory) {
+      return db[owner][name].participants[db[owner][name].winner-1].name;
+   }
+   function getTournamentParticipants(string memory name,address owner) public view returns(Team[] memory) {
+    return db[owner][name].participants;
    }
 
-   function getTournamentWinner (string memory name) public view returns (string memory) {
-      return db[name].participants[db[name].winner-1].name;
+   function getTournamentMatchups(string memory name, address owner) public view returns(Match[] memory) {
+      Match[] memory matches = new Match[](db[owner][name].visible);
+      for(uint i=0;i<db[owner][name].visible;i++)
+      {
+         Match storage member = db[owner][name].matchups[i];
+         matches[i]=member;
+      }
+      return matches;
    }
-   function viewTournamentName (string memory name) public view returns (string memory) {
-      return db[name].name;
-   }
-   function getTournamentParticipants(string memory name) public view returns(Team[] memory) {
-    return db[name].participants;
-   }
-
-   function getTournamentMatchups(string memory name) public view returns(Match[] memory) {
-      return db[name].matchups;
-   }
-
    function getTournaments (address owner) public view returns (string[] memory) {
       return tournaments[owner];
    }
+
+   function getMatch(uint256 id, address owner, string memory tname) public view returns (Match memory) {
+      return db[owner][tname].matchups[id];
+   }
    
-   function  createTournament(string memory name, string[] memory teams) public {
-      
-      uint i=0;
-      uint preskipped=0;
-      teams=shuffle(teams);
-      while(i<teams.length)
-      {
-        db[name].participants.push(Team(i+1,teams[i]));
-         i++;
-      }
-     i = 2;
-     
-     while(i<teams.length)
-     i=i*2;
-     db[name].initial=i/2;
-     preskipped=i-teams.length;
-     db[name].name=name;
-     
-    uint256 j;
-    for(j=0;j<i-1;j++)
+    function createTournament(string memory name, string[] memory teams)
+        public
     {
-      db[name].matchups.push(Match(j,0,0,0));
-    }
-      for( j=0; j<i/2;j++){
+      Bracket storage newBracket = db[msg.sender][name];
+      uint256 i=0;
+      uint256 preskipped;
+       for (i = 2; i < teams.length; i *= 2) {}
+        newBracket.initial = i / 2;
+      
+      preskipped = i - teams.length;
+      newBracket.name = name;
+      newBracket.visible=newBracket.initial+preskipped;
+
+      for(uint j = 0; j < teams.length; j++) {
+         newBracket.participants.push(Team(j + 1, teams[j]));
+         
          if(j<preskipped)
          {
-            db[name].matchups[j]=Match(j,j+1,0,j+1);
-            if(db[name].matchups[j/2+i/2].teama==0)
-            db[name].matchups[j/2+i/2].teama=j+1;
+            newBracket.matchups[j].teama=i+1;
+            newBracket.matchups[j].teamb=0;
+            newBracket.matchups[j].winner=i+1;
+
+            if(newBracket.matchups[(j/2)+newBracket.initial].teama==0)
+               newBracket.matchups[(j/2)+newBracket.initial].teama=j;
             else
-            db[name].matchups[j/2+i/2].teamb=j+1;
+               newBracket.matchups[(j/2)+newBracket.initial].teamb=j;
          }
-         else
-            db[name].matchups[j]=(Match(j,(j*2)+1-preskipped,(j*2)+2-preskipped,0));
+         else {
+            if(newBracket.matchups[((j-preskipped)/2)+preskipped].teama==0)
+               newBracket.matchups[((j-preskipped)/2)+preskipped].teama=j;
+            else
+               newBracket.matchups[((j-preskipped)/2)+preskipped].teamb=j;
+         }
+
       }
       tournaments[msg.sender].push(name);
-      db[name].creator=msg.sender;
-   }
+    }
 
-   function matchResult(string calldata tName, uint256 matchid, uint256 winner) public {
-      require(db[tName].creator==msg.sender);
-      require((db[tName].initial*2-1)>matchid);
-      require(db[tName].matchups[matchid].winner==0);
-      require(db[tName].matchups[matchid].teama!=0 && db[tName].matchups[matchid].teamb!=0);
-      require(db[tName].matchups[matchid].teama==winner|| db[tName].matchups[matchid].teamb==winner);
-      db[tName].matchups[matchid].winner=winner;
-      if(matchid==(db[tName].initial*2)-2){
-         db[tName].winner=winner;
-         return;
-      }
-      
-      uint256 k = 0;
-      uint256 i = matchid;
-      uint256 j=db[tName].initial;
-      while(i>=j)
-      {
-         k=k+j;
-         i=i-j;
-         j=j/2;
-      }
-      k=k+j;
-      uint256 nextId =i/2+k;  
-      if(db[tName].matchups[nextId].teama==0)
-         db[tName].matchups[nextId].teama=winner;
-      else 
-         db[tName].matchups[nextId].teamb=winner;
-   }   
+    function matchResult(
+        string calldata tName,
+        uint256 matchid,
+        uint256 winner
+    ) public {
+        require(db[msg.sender][tName].initial * 2 - 1 > matchid);
+        require(db[msg.sender][tName].matchups[matchid].winner == 0);
+        require(
+            winner == db[msg.sender][tName].matchups[matchid].teama ||
+                winner == db[msg.sender][tName].matchups[matchid].teamb
+        );
+        db[msg.sender][tName].matchups[matchid].winner = winner;
+        if (matchid != db[msg.sender][tName].initial * 2 - 2) {
+            uint256 k = 0;
+            uint256 i = matchid;
+            uint256 j = db[msg.sender][tName].initial;
+            for (; i >= j; ) {
+                k = k + j;
+                i = i - j;
+                j = j / 2;
+            }
+            uint256 nextId = i / 2 + k;
+            if (db[msg.sender][tName].matchups[nextId].teama == 0) {
+               db[msg.sender][tName].visible++;
+                db[msg.sender][tName].matchups[nextId].teama = winner;
+            } else {
+                db[msg.sender][tName].matchups[nextId].teamb = winner;
+            }
+        } else {
+            db[msg.sender][tName].winner = winner;
+        }
+        db[msg.sender][tName].visible++;
+    }
 }
